73faa0fe46ec624643cb0a8b7458b232cfd87a21
chemiczny <glanowskimichal@gmail.com>
2019-01-03 15:16:30 +0100

    Add file printing aliases - easier installation

---
+++ printAliases.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 15:08:05 2019
+
+@author: michal
+"""
+
+from os import getcwd
+from os.path import join
+
+cwd = getcwd()
+file2alias = { "sbatchPy.py" : "sbatchPy", "squeuePy.py" : "squeuePy", "sremove.py" : "sremove" }
+
+for script in file2alias:
+    path = join(cwd, script)
+    print("alias "+file2alias[script]+"='python "+path+"'")
