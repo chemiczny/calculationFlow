77242d102c904a46525a794538f100d899e89941
chemiczny <glanowskimichal@gmail.com>
2019-10-29 10:59:50 +0100

    Add generation of different PCM models

---
+++ graphSCRF.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 10:51:40 2019
+
+@author: michal
+"""
+
+from os.path import join
+from jobNode import GaussianNode
+from graphManager import GraphManager
+import sys
+
+def addSCRF(graph, node, theoryLow = "B3LYP/6-31G(d,p)", basename = "", additionalRoute = ""):
+    newDir = join(node, basename+"CPCM")
+    newNode = GaussianNode("cpcm.inp", newDir)
+    newNode.routeSection = """%Chk=checkp.chk
+%Mem=100GB
+#P """+theoryLow+"""
+# nosymm SCRF(CPCM, Solvent=Generic, Read) """+additionalRoute+"""
+# Gfinput IOP(6/7=3)  Pop=full  Density  Test 
+# Units(Ang,Deg)
+"""
+    newNode.additionalSection = """
+stoichiometry=H2O1
+solventname=Water2
+eps=4
+epsinf=1.77556
+"""
+    newNode.verification = "SP"
+    newNode.readResults = True
+    graph.add_node(newDir, data = newNode)
+    graph.add_edge(node, newDir)
+    
+    newDir = join(node, basename+"CPCM")
+    newNode = GaussianNode("smd.inp", newDir)
+    newNode.routeSection = """%Chk=checkp.chk
+%Mem=100GB
+#P """+theoryLow+"""
+# nosymm SCRF(SMD, Solvent=Generic, Read) """+additionalRoute+"""
+# Gfinput IOP(6/7=3)  Pop=full  Density  Test 
+# Units(Ang,Deg)
+"""
+    newNode.additionalSection = """
+stoichiometry=H2O1
+solventname=Water2
+eps=4
+epsinf=1.77556
+"""
+    newNode.verification = "SP"
+    newNode.readResults = True
+    graph.add_node(newDir, data = newNode)
+    graph.add_edge(node, newDir)
+    
+    graph.nodes[node]["data"].status = "finished"
+    
+    return graph
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: graphSCRF node")
+    elif len(sys.argv) == 2:
+        node = sys.argv[1]
+        
+        sm = GraphManager()
+        graph  = sm.isGraphHere(node)
+        if graph:
+            graph = addSCRF(node)
+            sm.saveGraphs()
+        else:
+            print("Invalid node key!")
+        
+    else:
+        print( "cooooo?")
